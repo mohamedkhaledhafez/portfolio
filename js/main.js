7ba598d80c8e841e0be72a8ec0af0193c55bbd49
mohamedkhaledhafez <mohamedkhaled97858@gmail.com>
2022-01-12 02:11:30 +0200

    change portfolio section and services text colors and fixed some issues

--- js/main.js
+++ js/main.js
@@ -147,7 +147,7 @@ const getCurrentTheme = () =>
 const getCurrentIcon = () =>
   themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
 
-// We validate if the user previously chose a topic
+// We validate if the user previously choose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
   document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
