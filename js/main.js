4e4b606d2ada92cc0bdad822abb3339e7a3dbec6
mohamedkhaledhafez <mohamedkhaled97858@gmail.com>
2026-01-23 02:48:35 +0200

    Fix an issue at My Qualifications section

--- js/main.js
+++ js/main.js
@@ -49,6 +49,29 @@ skillsHeader.forEach((elem) => {
   elem.addEventListener("click", toggleSkillsIcon);
 });
 
+/*===================== QUALIFICATION TABS=====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target)
+
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('qualification__active')
+    })
+    target.classList.add('qualification__active')
+
+    tabs.forEach(tab => {
+      tab.classList.remove('qualification__active')
+    })
+    tab.classList.add('qualification__active')
+
+  })
+})
+
+
+
 /*===================== Active Services Modal ==================*/
 
 const modalViews = document.querySelectorAll(".services__modal"),
